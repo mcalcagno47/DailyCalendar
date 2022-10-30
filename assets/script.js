dd6ae5933dd4096a24d52830c60f576bc5c1cd2a
Mark Calcagno <mcalcagn@gmail.com>
2022-10-30 14:14:59 -0500

    added logic for classes in javascript. attempting to get time to appear in jumbotron

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
-$(document).ready(function (){
-    
-    function saveBtn() {}
+$(document).ready(function () {
 
-    function hourlyUpdate() {}
+    $("saveBtn").click(function () {
+        var time = $(this).parent().attr("id");
+        var description = $(this).siblings(".description").val();
+
+        localStorage.setItem(time, description);
+    });
+
+    function hourlyUpdate() {
+        var currentHour = hours();
+
+        var previousHour = parseInt($(this).attr("id").split("-")[1])
+
+        $(".time-block").each(function () {
+            if (currentHour === previousHour) {
+                (".time-block").addClass("present");
+            } else if (currentHour > previousHour) {
+                (".time-block").addClass("future");
+            } else {
+                (".time-block").addClass("past");
+            }
+        });
+    };
 
     hourlyUpdate();
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do"))
 
 });
