3d4beff8772452df50bd058d2b4d16a0bf05d790
Mark Calcagno <mcalcagn@gmail.com>
2022-10-30 16:40:04 -0500

    received help from friend/tutor, calendar is working.  Want to know if I can do it my original way still...

--- assets/script.js
+++ assets/script.js
@@ -7,26 +7,35 @@ $(document).ready(function () {
         localStorage.setItem(time, description);
     }));
 
-    function hourlyUpdate() {
+    function timeCheck() {
         var currentHour = moment().hours();
 
         $(".time-block").each(function () {
-            var previousHour = parseInt($(this).attr("id").split("-")[1])
+            var previousHour = parseInt($(this).attr("id").split("-")[1]);
 
             if (currentHour === previousHour) {
                 $(this).addClass("present");
             } else if (currentHour > previousHour) {
-                $(this).addClass("future");
-            } else {
                 $(this).addClass("past");
+            } else {
+                $(this).addClass("future");
             }
         });
     };
 
-    hourlyUpdate();
+    timeCheck();
 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"))
+    var updater = setInterval(timeCheck, 15000);
 
-    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#currentDay").text(moment().format("dddd, MMMM Do, hh:mm"))
 
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 });
